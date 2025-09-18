aa6ff5e0d7493669907611a3b99e2eec13d46fe5
Christine Matta <christine.onsi@gmail.com>
2025-09-18 13:15:13 +0200

    Add newsletter Component

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
-import './NewsLetter.css'
+import React from "react";
+import "./NewsLetter.css";
+import newsletter from "../Assets/newsletter.jpg";
+import { IoArrowRedoCircleSharp } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 const NewsLetter = () => {
   return (
-    <div className='newsletter-container'>
+    <div className="newsletter-container">
       <div className="newsletter-left">
-        <img src="" alt="" />
+        <img src={newsletter} alt="" />
       </div>
-      <div className="newsletter-right"></div>
+      <div className="newsletter-right">
+        <div className="newsletter-title">
+          <p className="sub-title">First Time?</p>
+          <h2 className="title">Sign up and get 20% off your first order</h2>
+        </div>
 
+        <div className="email-box">
+          <input
+            type="email"
+            className="email-input"
+            id=""
+            placeholder="Enter your email"
+          ></input>
+          <button className="email-btn">
+            <IoArrowRedoCircleSharp />
+          </button>
+        </div>
+        <p>
+          Subscribe and be the first to hear about new arrivals, special
+          promotions and online exclusives.{" "}
+        </p>
+        <div className="social-media-icons">
+          <a href="https://www.facebook.com/YourPage" target="_blank" rel="noopener noreferrer">
+    <FaFacebookF />
+  </a>
+
+  <a href="https://twitter.com/YourProfile" target="_blank" rel="noopener noreferrer">
+    <FaXTwitter />
+  </a>
+
+  <a href="https://www.instagram.com/YourProfile" target="_blank" rel="noopener noreferrer">
+    <FaInstagram />
+  </a>
+
+  <a href="https://www.youtube.com/YourChannel" target="_blank" rel="noopener noreferrer">
+    <FaYoutube />
+  </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
