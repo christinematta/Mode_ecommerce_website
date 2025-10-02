707a3379795e2f9899a22e9618bb6b08964599a0
Christine Matta <christine.onsi@gmail.com>
2025-10-02 14:02:28 +0200

    add links to breadcrum

--- src/Components/Breadcrum/Breadcrum.jsx
+++ src/Components/Breadcrum/Breadcrum.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Breadcrum.css";
 import { IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Breadcrum = ({ product }) => {
   return (
     <div className="breadcrum-list">
-      <p>HOME</p>
+      <Link to="/">HOME</Link>
       <IoIosArrowForward />
-      <p>SHOP</p>
+      <Link to="/"> SHOP</Link>
       <IoIosArrowForward />
-      <p>{product.category}</p>
+       <Link to={`/${product.category}`}>
+        {product.category}
+      </Link>
+
       <IoIosArrowForward />
       <p>{product.name}</p>
     </div>
