53aaa81a0765088a4bbecb66621ecbdd3a12b0dd
Christine Matta <christine.onsi@gmail.com>
2025-10-09 13:30:17 +0200

    Build a default cart, Add item to cart,Remove item from cart,Get total number of items in cart

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from "react";
+import { React, useContext, useRef } from "react";
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import { IoIosArrowDown } from "react-icons/io";
@@ -6,10 +6,17 @@ import Item from "../Components/Item/Item";
 
 const ShopCategory = (props) => {
   const { all_Product } = useContext(ShopContext);
+  const listRef = useRef(null);
 
   const filtered_category = all_Product.filter(
     (item) => item.category === props.category
   );
+
+  const handleScroll=()=>{
+    if(listRef.current){
+      listRef.current.scrollIntoView({behaviour:"smooth"})
+    }
+  }
   return (
     <div className="shop-category">
       <div className="shopcategory-banner">
@@ -21,10 +28,10 @@ const ShopCategory = (props) => {
             <h3>Extra 20% off</h3>
           </div>
 
-          <button className="category-btn">Explore Now</button>
+          <button className="category-btn" onClick={handleScroll}>Explore Now</button>
         </div>
       </div>
-      <div className="shopcategory-listitem">
+      <div className="shopcategory-listitem" ref={listRef}>
         <div className="category-sort">
           <p>
             <span>Showing 1-12</span> out of 36 products.
